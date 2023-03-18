a4a19c33ca92a052d507c2852c0f59b761be6977
root <root@DESKTOP-JCQ5PVO.Home>
2023-03-18 06:05:34 +0000

    go

---
+++ test_GO/even_odd.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+"fmt"
+)
+
+func main(){
+
+num := 14
+
+if num % 2 == 0 { //checks if number is even
+
+	
+	fmt.Println("the number", num, "is even")
+
+}else{
+
+	fmt.Println("the number", num, "is odd")
+}
+
+}
---
+++ test_GO/loop.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+
+	"fmt"
+
+)
+
+func main() {
+
+	x := 9
+
+	if x < 10 {
+
+		fmt.Println(x)
+	}
+
+	else{
+		fmt.Println("nothing")
+	}
+
+}
